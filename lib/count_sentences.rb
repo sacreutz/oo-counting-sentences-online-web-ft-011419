1ac1702571534011541de079ab77a40796f02f38
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-08-14 21:53:05 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     split_everything = self.split(/\W+/)
-    split_everything = split_everything.reject { |c| c.empty? }
+    split_everything.delete_if{|e| e.length == 0}
     split_everything.count
   end
 end
