0b60998f7bc5b8bc0cd41011e7c746c093c82104
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-08-14 21:56:29 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_everything = self.split(/\W+/, 4)
+    split_everything.delete_if{|e| e.length == 0}
+    split_everything.count
   end
 end
